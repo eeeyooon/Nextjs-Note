8fedceeee7faecb4829980936a317a2140a805ed
jiyoon <vywns9978@gmail.com>
2023-09-13 17:31:58 +0900

    generateStaticParams 사용하여 페이지 미리 생성

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -9,3 +9,10 @@ type Props = {
 export default function PantsPage({ params }: Props) {
   return <h1>{params.slug} 제품 설명</h1>;
 }
+
+export function generateStaticParams() {
+  const products = ["pants", "skirt"];
+  return products.map((product) => ({
+    slug: product,
+  }));
+}
