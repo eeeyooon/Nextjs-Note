3c021091538b8c141945aab64d076056bd6d8c4f
jiyoon <vywns9978@gmail.com>
2023-09-15 15:47:22 +0900

    link practice

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link";
 import React from "react";
+import styles from "./layout.module.css";
+
+const products = ["shirt", "pants", "skirt", "shoes"];
 
 export default function ProductsPage() {
-  return <h1>제품 소개 페이지!</h1>;
+  return (
+    <>
+      <h1>제품 소개 페이지!</h1>
+      <ul className={styles.ul}>
+        {products.map((product, index) => (
+          <li key={index}>
+            <Link href={`/products/${product}`}>{product}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
