411d5f849b8d97f5e0893f29a21f97554793b844
jiyoon <vywns9978@gmail.com>
2023-10-09 16:43:37 +0900

    Server Component vs Client Component

---
+++ src/components/Counter.tsx
@@ -0,0 +1,15 @@
+"use client"; // Client Component라는 것을 명시.
+
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => setCount((num) => num + 1)}>
+        숫자 증가 시키기
+      </button>
+    </>
+  );
+}
