cd22332cc4362919238511ae242997c68314f049
jiyoon <vywns9978@gmail.com>
2023-10-24 23:44:01 +0900

    [🔨 refactor] 수정한 함수에 따라 타입 명시 및 비동기 함수 코드로 변경

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getProduct, getProducts } from "@/service/products";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -8,28 +9,34 @@ type Props = {
   };
 };
 
-export function generateMetadata({ params }: Props) {
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { slug } = params;
+  const product = await getProduct(slug);
+
   return {
-    title: `제품의 이름: ${params.slug}`,
+    title: `Wonderful Items | ${product?.name.toUpperCase()}`,
+    description: `Check our wonderful ${product?.name}!`,
   };
-}
+};
 
 // prams.slug만 사용중이기 때문에 params를 구조분해할당해서 slug만 뽑음.
-export default function PantsPage({ params: { slug } }: Props) {
-  const product = getProduct(slug);
+export default async function ProductPage({ params: { slug } }: Props) {
+  const product = await getProduct(slug);
 
   if (!product) {
     notFound();
   }
 
   // 서버 파일에 있는 데이터 중 해당 제품의 정보를 찾아서 그걸 보여주도록 변경할 것.
-  return <h1>{product} 제품 설명</h1>;
+  return <h1>{product.name} 제품 설명</h1>;
 }
 
-export function generateStaticParams() {
+export async function generateStaticParams() {
   // 모든 제품의 페이지들을 미리 만들어둘 수 있게 해줄 거임. (SSG)
-  const products = getProducts();
+  const products = await getProducts();
   return products.map((product) => ({
-    slug: product,
+    slug: product.id,
   }));
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 import React from "react";
 import styles from "./layout.module.css";
 
-export default function ProductsPage() {
+export default async function ProductsPage() {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서 그걸 보여주도록 변경할 예정.
 
   // 만들어놓은 getProducts() 사용
-  const products = getProducts();
+  const products = await getProducts();
 
   return (
     <>
       <ul className={styles.ul}>
         <h1>제품 소개 페이지!</h1>
-        {products.map((product, index) => (
+        {products.map(({ id, name }, index) => (
           <li key={index}>
-            <Link href={`/products/${product}`}>{product}</Link>
+            <Link href={`/products/${id}`}>{name}</Link>
           </li>
         ))}
       </ul>
